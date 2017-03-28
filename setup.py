b24858ad4b5084a3176cc85ec9537f1d77089eda
Matt Bauman <mbauman@gmail.com>
2017-03-28 12:44:49 -0500

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='superdeduper',
-    version='0.1.4',
+    version='0.1.5',
     description="A simple interface to datamade/dedupe to make probabilistic record linkage easy.",
     long_description=readme + '\n\n' + history,
     author="DSaPP Researchers",
--- superdeduper/__init__.py
+++ superdeduper/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """DSaPP Researchers"""
 __email__ = 'datascifellows@gmail.com'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
