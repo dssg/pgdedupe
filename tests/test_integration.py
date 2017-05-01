fb326bab67f45b34b2927da539a7e74a6ae71ed6
Matt Bauman <mbauman@gmail.com>
2017-05-01 18:19:44 -0500

    Make testing scripts a little nicer and more consistent
    Should resolve #33.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,46 +2,29 @@
 from pgdedupe import cli
 from click.testing import CliRunner
 import tests.generate_fake_dataset as gen
+import tests.initialize_db as initdb
 
 import os
-import json
+import yaml
 import psycopg2 as psy
 import testing.postgresql
 
+
 def test_integration():
     psql = testing.postgresql.Postgresql()
-    con = psy.connect(**psql.dsn())
-    c = con.cursor()
+    with open('db.yaml','w') as f:
+        yaml.dump(psql.dsn(), f)
 
     pop = gen.create_population(2000)
     gen.create_csv(pop, 'pop.csv')
 
-    c.execute("""
-         CREATE TABLE entries (
-             uuid UUID,
-             first_name VARCHAR,
-             last_name VARCHAR,
-             ssn VARCHAR(11),
-             sex VARCHAR(1),
-             dob VARCHAR(10),
-             race VARCHAR,
-             ethnicity VARCHAR
-         );""")
-    con.commit()
-    with open('pop.csv') as f:
-        f.readline() # skip the header
-        c.copy_from(f, 'entries', sep=',', null='')
-    c.execute("ALTER TABLE entries ADD COLUMN entry_id SERIAL PRIMARY KEY;")
-    c.execute("ALTER TABLE entries ADD COLUMN full_name VARCHAR;")
-    c.execute("UPDATE entries SET full_name = first_name || ' ' || last_name;")
-    con.commit()
-
-    with open('db.json','w') as f:
-        json.dump(psql.dsn(), f)
+    initdb.init('db.yaml', 'pop.csv')
     
     runner = CliRunner()
-    result = runner.invoke(cli.main, ['--config', 'config.yaml', '--db', 'db.json'])
+    result = runner.invoke(cli.main, ['--config', 'config.yaml', '--db', 'db.yaml'])
     assert result.exit_code == 0
 
-    c.execute("SELECT count(distinct dedupe_id) FROM entries")
+    con = psy.connect(**psql.dsn())
+    c = con.cursor()
+    c.execute("SELECT count(distinct dedupe_id) FROM dedupe.entries")
     assert(c.fetchone()[0] < 4000) # Rudimentary quality check
